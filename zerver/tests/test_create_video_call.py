698d7f24cd89eb136cf31072757b708b70ea80d9
Fabian Grutschus <github@lubyte.de>
2026-03-30 18:15:54 +0200

    Get realm id from user

--- zerver/tests/test_create_video_call.py
+++ zerver/tests/test_create_video_call.py
@@ -566,7 +566,6 @@ class BigBlueButtonVideoCallTest(ZulipTestCase):
                             "name": "general > meeting",
                             "lock_settings_disable_cam": False,
                             "moderator": self.user.id,
-                            "realm_id": self.user.realm.id,
                         }
                     ),
                 ),
@@ -588,7 +587,6 @@ class BigBlueButtonVideoCallTest(ZulipTestCase):
                             "name": "general > meeting",
                             "lock_settings_disable_cam": True,
                             "moderator": self.user.id,
-                            "realm_id": self.user.realm.id,
                         }
                     ),
                 ),
